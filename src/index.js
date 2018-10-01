4c6312ef503e519282071c0e1e37f91d8b5ff977
Mpot7 <r-kisa@mail.ru>
2018-10-01 09:48:20 +0300

    First commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,37 @@
 // PLEASE DON'T change function name
-module.exports = function makeExchange(currency) {
+module.exports = function makeExchange(currency) 
+{
     // Your code goes here!
     // Return an object containing the minimum number of coins needed to make change
+
+var ob={}, val;
+var coins=[50, 25, 10, 5, 1];
+var nom=["H","Q","D","N","P"];
+console.log (currency);
+if (currency>=0)
+ {
+  if (currency<=10000)
+   {
+     for (i=0; i<5; i++)
+      {
+
+      val=Math.floor(currency/coins[i]);
+      if (val>0)
+        {
+          ob[nom[i]]=val;
+          currency=currency % coins[i];
+         }
+      
+       }
+
+
+ 
+    }
+  else
+   {
+    ob={error: "You are rich, my friend! We don't have so much coins for exchange"};
+    }
+ }
+//console.log (ob);
+return ob;
 }
